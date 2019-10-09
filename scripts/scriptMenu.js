b3fa6cd0b0b4749c6c8d2473e8a6e397e09682ad
constant gillet <constant.gillet@hetic.net>
2019-10-10 01:02:47 +0200

    adding menu

---
+++ scripts/scriptMenu.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.menuDiv')
+const buttonOpenMenu = document.querySelector('header--ButtonMenu')
+let menuDisplayed = false;
+
+addEventListener('click', function(){
+    if (menuDisplayed) {
+        
+    } else {
+        
+    }
+})
+
