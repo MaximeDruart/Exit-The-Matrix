ca458edca0332386cad24636d71069d5a86a9bed
cgillet <constant.gillet@hetic.net>
2019-10-10 14:41:25 +0200

    adding videos images

--- scripts/scriptVideo.js
+++ scripts/scriptVideo.js
@@ -1,8 +1,10 @@
 const extracts = document.querySelectorAll('.extracts-section__sceneImage')
 const playerDiv = document.querySelector('.playerDiv')
 const video = document.querySelector('.playerDiv__video')
+const closeButton = document.querySelector('.playerDiv__close')
 let playingVideo = false
 let playerDivDisplayed = false
+const videoSource = document.querySelector('.playerDiv__video>source')
 
 for (let i = 0; i < extracts.length; i++) {
     
@@ -11,6 +13,8 @@ for (let i = 0; i < extracts.length; i++) {
         playerDiv.classList.add('displayPlayerDiv')
         playerDivDisplayed = true
 
+        loadVideo(i)
+        
         video.play()
         playingVideo = true
                   
@@ -32,3 +36,38 @@ video.addEventListener('click', function(){
     }
 })
 
+closeButton.addEventListener('click', function(){   
+    
+    playerDiv.classList.remove('displayPlayerDiv')
+    playerDivDisplayed = true
+})
+
+function loadVideo(number){
+
+    switch (number) {
+        case 0:
+
+            videoSource.setAttribute('src', 'videos/extract_Pill.mp4')
+            break;
+        case 1:
+
+            videoSource.setAttribute('src', 'videos/extract_KungFu.mp4')
+            break
+        case 2:
+
+            videoSource.setAttribute('src', 'videos/extract_MrSmith.mp4')
+            break
+        case 3:
+
+            videoSource.setAttribute('src', 'videos/extract_WakeUp.mp4')
+            break
+        case 4:
+
+            videoSource.setAttribute('src', 'videos/extract_BulletTime.mp4')
+            break
+        default:
+            break;
+    }
+
+    video.load();
+}
